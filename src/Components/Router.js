63318aac0d2a1e23e2c016eacb08a0b50ac80748
hacsoo2 <taekseung2@gmail.com>
2019-09-20 17:48:47 +0900

    nomad react tutorial part 3

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home"
-import Search from "Routes/Search";
 import TV from "Routes/TV";
+import Header from "Components/Header";
+import Search from "Routes/Search";
 
 export default () => (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/tv" component={TV} />
-        <Route path="/search" component={Search} />
-        <Redirect from="*" to="/" />
-      </Switch>
+      <>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/tv" component={TV} />
+          <Route path="/search" component={Search} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </>
     </Router>
 );
