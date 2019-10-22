23517a17fe6f8c954e35a997c109884ea23593f2
hacsoo <wutac34@gmail.com>
2019-10-22 22:23:12 +0900

    edit typo

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ const api = axios.create({
     }
 });
 
-export const movieApi = {
+export const moviesApi = {
     nowPlaying: () => api.get("movie/now_playing"),
     upcoming: () => api.get("movie/upcoming"),
     popular: () => api.get("movie/popular"),
